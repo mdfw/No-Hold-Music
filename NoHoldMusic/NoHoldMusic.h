99b7cb2520e3120f5ccfb4e2b82fee2127261e5b
Mark Williams <github@shymedia.com>
2016-04-28 21:51:15 -0700

    Initial Commit

---
+++ NoHoldMusic/NoHoldMusic.h
@@ -0,0 +1,19 @@
+//
+//  NoHoldMusic.h
+//  NoHoldMusic
+//
+//  Created by Mark D. Freeman Williams on 4/28/16.
+//  Copyright © 2016 The Fascinating Group. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for NoHoldMusic.
+FOUNDATION_EXPORT double NoHoldMusicVersionNumber;
+
+//! Project version string for NoHoldMusic.
+FOUNDATION_EXPORT const unsigned char NoHoldMusicVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NoHoldMusic/PublicHeader.h>
+
+
---
+++ NoHoldMusicTests/NoHoldMusicTests.m
@@ -0,0 +1,39 @@
+//
+//  NoHoldMusicTests.m
+//  NoHoldMusicTests
+//
+//  Created by Mark D. Freeman Williams on 4/28/16.
+//  Copyright © 2016 The Fascinating Group. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NoHoldMusicTests : XCTestCase
+
+@end
+
+@implementation NoHoldMusicTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
