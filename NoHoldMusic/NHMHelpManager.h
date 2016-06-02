cdaf4ae0cc491c7b333857753f141f7a98282e72
Mark Williams <github@shymedia.com>
2016-06-01 22:10:51 -0700

    Pulled in API headers from the NSHelpManager class that we are going to try to copy.

--- NoHoldMusic/NHMHelpManager.h
+++ NoHoldMusic/NHMHelpManager.h
@@ -10,12 +10,20 @@
 
 @interface NHMHelpManager : NSObject
 
-+ (nonnull id)sharedHelpSystem;
++ (nonnull id)sharedHelpManager;
+
+
+- (void)openHelpAnchor:(nonnull NSString *)anchor inBook:(nullable NSString *)book;
+- (void)findString:(nonnull NSString *)query inBook:(nullable NSString *)book;
+
+/* Register one or more help books in the given bundle.  The main bundle is automatically registered by -openHelpAnchor:inBook: and -findString:inBook:.  You can use -registerBooksInBundle: to register help books in a plugin bundle, for example.  The Info.plist in the bundle should contain a help book directory path, which specifies one or more folders containing help books.  Returns NO if the bundle doesn't contain any help books or if registration fails.  Returns YES on successful registration. */
+- (BOOL)registerBooksInBundle:(nonnull NSBundle *)bundle;
+
 
 /**
  *  Show the help window.
  *
  *  @param sender The object requesting the window.
  */
-- (IBAction)showHelpWindow:(nullable id)sender;
+- (IBAction)showHelp:(nullable id)sender;
 @end
